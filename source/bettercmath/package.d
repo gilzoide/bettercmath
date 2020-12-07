41ba7941a4d9d68e0902d22e0dc297b75bb668b5
gilzoide <gilzoide@gmail.com>
2020-12-07 19:45:43 -0300

    Add angles to package.d, reorder imports

--- source/bettercmath/package.d
+++ source/bettercmath/package.d
@@ -1,6 +1,7 @@
 module bettercmath;
 
+public import bettercmath.angles;
 public import bettercmath.cmath;
 public import bettercmath.hexagrid2d;
-public import bettercmath.vector;
 public import bettercmath.matrix;
+public import bettercmath.vector;
