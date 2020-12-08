656b01634d040d85d3277cbd0535ed3d0b388d1e
gilzoide <gilzoide@gmail.com>
2020-12-08 20:06:19 -0300

    WIP Add easings submodule

--- source/bettercmath/package.d
+++ source/bettercmath/package.d
@@ -1,6 +1,7 @@
 module bettercmath;
 
 public import bettercmath.cmath;
+public import bettercmath.easings;
 public import bettercmath.hexagrid2d;
 public import bettercmath.matrix;
 public import bettercmath.misc;
