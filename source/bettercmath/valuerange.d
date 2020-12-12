3caaf5d1edb15cbc3f343c45360d1c60047ad17a
gilzoide <gilzoide@gmail.com>
2020-12-11 23:41:24 -0300

    Add some constructors to ValueRange and method to invert them

--- source/bettercmath/valuerange.d
+++ source/bettercmath/valuerange.d
@@ -16,6 +16,30 @@ struct ValueRange(T)
 {
     T from = 0, to = 1;
 
+    this(const T from, const T to)
+    {
+        this.from = from;
+        this.to = to;
+    }
+    this(const T[2] values)
+    {
+        from = values[0];
+        to = values[1];
+    }
+
+    ref ValueRange invert()
+    {
+        import std.algorithm : swap;
+        swap(from, to);
+        return this;
+    }
+
+    ValueRange inverted() const
+    {
+        typeof(return) r = this;
+        return r.invert();
+    }
+
     T lerp(U)(const U amount) const
     {
         return .lerp(from, to, amount);
