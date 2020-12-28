4b0f9626786c64ff4e4f13c1b07bf7ee0fcd8cfd
gilzoide <gilzoide@gmail.com>
2020-12-27 22:19:05 -0300

    Add TransformOptions, add copyInto and opCast for Transforms, remove full/compact/fullInto/compactInto, add some documentation to transform.d

--- source/bettercmath/transform.d
+++ source/bettercmath/transform.d
@@ -1,113 +1,132 @@
 module bettercmath.transform;
 
-import bettercmath.cmath : cos, sin;
-import bettercmath.matrix;
-import bettercmath.misc : FloatType, degreesToRadians;
+/// Options for the Transform template.
+enum TransformOptions
+{
+    /// Default options.
+    none = 0,
+    /// Use a compact Matrix type, as Affine Transformation matrices always
+    /// have the last row [0 ... 0 1]
+    compact = 1 << 0,
+}
 
 @nogc @safe pure nothrow:
 
-struct Transform(T, uint Dim, bool _isCompact = false)
+/**
+ * Affine Transformation matrix.
+ *
+ * Params:
+ *   T = Value types, should be numeric
+ *   Dim = Space dimensions, pass 2 for 2D, 3 for 3D, etc...
+ *   options = Additional options
+ */
+struct Transform(T, uint Dim, TransformOptions options = TransformOptions.none)
 if (Dim > 0)
 {
+    import std.algorithm : min;
+
+    import bettercmath.cmath : cos, sin;
+    import bettercmath.matrix : Matrix;
+    import bettercmath.misc : FloatType, degreesToRadians;
+
     alias ElementType = T;
+    /// Transform dimension.
     enum dimension = Dim;
-    enum isCompact = _isCompact;
+    /// Whether Transform is compact.
+    enum bool isCompact = options & TransformOptions.compact;
     static if (!isCompact)
     {
+        /// The underlying matrix type
         alias MatrixType = Matrix!(T, Dim + 1, Dim + 1);
-        alias CompactTransform = Transform!(T, Dim, true);
-        alias FullTransform = typeof(this);
+        private alias CompactTransform = Transform!(T, Dim, TransformOptions.compact);
 
-        ref CompactTransform.MatrixType compactInto(ref return CompactTransform.MatrixType mat) inout
-        {
-            foreach (i; 0 .. Dim + 1)
-            {
-                mat[i][0 .. Dim] = this.matrix[i][0 .. Dim];
-            }
-            return mat;
-        }
-
-        CompactTransform compact() const
-        {
-            typeof(return) t = void;
-            compactInto(t.matrix);
-            return t;
-        }
-
-        static bool isAffineTransformMatrix(const MatrixType matrix)
+        private static bool isAffineTransformMatrix(const MatrixType matrix)
         {
             import std.algorithm : equal;
             import std.range : chain, only, repeat;
             return matrix.rows[Dim].equal(repeat(Dim, 0).chain(only(1)));
         }
-
-        auto opCast(U : CompactTransform)() const
-        {
-            return compact();
-        }
     }
     else
     {
+        /// The underlying matrix type
         alias MatrixType = Matrix!(T, Dim + 1, Dim);
-        alias CompactTransform = typeof(this);
-        alias FullTransform = Transform!(T, Dim, false);
-        
-        ref FullTransform.MatrixType fullInto(ref return FullTransform.MatrixType mat) inout
-        {
-            foreach (i; 0 .. Dim + 1)
-            {
-                mat[i][0 .. Dim] = this.matrix[i][0 .. Dim];
-                mat[i][Dim] = 0;
-            }
-            mat[Dim][Dim] = 1;
-            return mat;
-        }
+        private alias CompactTransform = typeof(this);
 
-        FullTransform full() const
-        {
-            typeof(return) t = void;
-            fullInto(t.matrix);
-            return t;
-        }
-
-        static bool isAffineTransformMatrix(const MatrixType matrix)
+        private static bool isAffineTransformMatrix(const MatrixType _)
         {
             return true;
         }
-
-        auto opCast(U : FullTransform)() const
-        {
-            return full();
-        }
     }
-    alias FT = FloatType!T;
+    private alias FT = FloatType!T;
 
+    /// Cast between Transform types of any dimension.
+    U opCast(U : Transform!(T, D, O), uint D, TransformOptions O)() const
+    {
+        typeof(return) result;
+        return copyInto(result);
+    }
+
+    /// Copy Transform contents into `target` transform of any dimension and options.
+    auto ref copyInto(uint D, TransformOptions O)(ref return Transform!(T, D, O) target) const
+    {
+        copyInto(target.matrix);
+        return target;
+    }
+
+    /// Copy Transform contents into a matrix of Transform-like dimensions.
+    auto ref copyInto(uint C, uint R)(ref return Matrix!(T, C, R) target) const
+    if (C == R || C == R + 1)
+    {
+        static if (target.rowSize == this.matrix.rowSize)
+        {
+            matrix.copyInto(target);
+        }
+        else
+        {
+            enum minDimension = min(C - 1, this.dimension);
+            foreach (i; 0 .. minDimension)
+            {
+                target[i][0 .. minDimension] = this.matrix[i][0 .. minDimension];
+            }
+            // translations must be in the last column, so copy them separately
+            target[$-1][0 .. minDimension] = this.matrix[$-1][0 .. minDimension];
+        }
+        return target;
+    }
+
+
+    /// The underlying matrix.
     MatrixType matrix = MatrixType.fromDiagonal(1);
     alias matrix this;
 
+    /// The identity Transform.
     enum identity = Transform.init;
 
-    this(const MatrixType matrix)
-    in { assert(isAffineTransformMatrix(matrix), "Matrix is not suitable for affine transformations"); }
+    /// Construct a Transform from matrix.
+    this()(const auto ref MatrixType mat)
+    in { assert(isAffineTransformMatrix(mat), "Matrix is not suitable for affine transformations"); }
     do
     {
-        this.matrix = matrix;
+        this.matrix = mat;
     }
 
+    /// Reset a Transform to identity.
     ref Transform setIdentity() return
     {
         this = identity;
         return this;
     }
 
-    T[N] transform(uint N)(const T[N] values) const
-    if (N <= Dim + 1)
+    /// Transform an array of values of any dimension.
+    T[N] transform(uint N)(const auto ref T[N] values) const
     {
+        enum minDimension = min(N, Dim + 1);
         typeof(return) result;
         foreach (i; 0 .. Dim)
         {
             T sum = 0;
-            foreach (j; 0 .. N)
+            foreach (j; 0 .. minDimension)
             {
                 sum += matrix[j, i] * values[j];
             }
@@ -119,109 +138,136 @@ if (Dim > 0)
         }
         return result;
     }
-    auto opBinary(string op : "*", uint N)(const T[N] values) const
-    if (N <= Dim + 1)
+    /// Transform an array of values of any dimension.
+    auto opBinary(string op : "*", uint N)(const auto ref T[N] values) const
     {
         return transform(values);
     }
 
-    static Transform fromTranslation(uint N)(const T[N] values)
-    if (N <= Dim)
+    /// Constructs a new Transform representing a translation.
+    static Transform fromTranslation(uint N)(const auto ref T[N] values)
     {
+        enum minDimension = min(N, Dim);
         Transform t;
-        t[$-1][0 .. N] = values[];
+        t[$-1][0 .. minDimension] = values[0 .. minDimension];
         return t;
     }
-    ref Transform translate(uint N)(const T[N] values) return
-    if (N <= Dim)
+    /// Apply translation in-place.
+    /// Returns: this
+    ref Transform translate(uint N)(const auto ref T[N] values) return
     {
-        this[$-1][0 .. N] += values[];
+        enum minDimension = min(N, Dim);
+        this[$-1][0 .. minDimension] += values[0 .. minDimension];
         return this;
     }
-    Transform translated(uint N)(const T[N] values) const
-    if (N <= Dim)
+    /// Returns a translated copy of Transform.
+    Transform translated(uint N)(const auto ref T[N] values) const
     {
         Transform t = this;
         return t.translate(values);
     }
 
-    static Transform fromScaling(uint N)(const T[N] values)
-    if (N <= Dim)
+    /// Constructs a new Transform representing a scaling.
+    static Transform fromScaling(uint N)(const auto ref T[N] values)
     {
+        enum minDimension = min(N, Dim);
         Transform t;
-        foreach (i; 0 .. N)
+        foreach (i; 0 .. minDimension)
         {
             t[i, i] = values[i];
         }
         return t;
     }
-    ref Transform scale(uint N)(const T[N] values) return
-    if (N <= Dim)
+    /// Apply scaling in-place.
+    /// Returns: this
+    ref Transform scale(uint N)(const auto ref T[N] values) return
     {
         return this.combine(CompactTransform.fromScaling(values));
     }
-    Transform scaled(uint N)(const T[N] values) const
+    /// Returns a scaled copy of Transform.
+    Transform scaled(uint N)(const auto ref T[N] values) const
     {
         Transform t = this;
         return t.scale(values);
     }
 
-    static Transform fromShearing(uint N)(const T[N] values)
-    if (N <= Dim)
-    {
-        Transform t;
-        foreach (i; 0 .. N)
-        {
-            foreach (j; 0 .. N)
-            {
-                if (j != i)
-                {
-                    t[j, i] = values[i];
-                }
-            }
-        }
-        return t;
-    }
-    ref Transform shear(uint N)(const T[N] values) return
-    if (N <= Dim)
-    {
-        return this.combine(CompactTransform.fromShearing(values));
-    }
-    Transform sheared(uint N)(const T[N] values) const
-    if (N <= Dim)
-    {
-        Transform t = this;
-        return t.shear(values);
-    }
-
     // 2D transforms
     static if (Dim >= 2)
     {
+        /// Constructs a new Transform representing a shearing.
+        static Transform fromShearing(uint N)(const auto ref T[N] values)
+        {
+            enum minDimension = min(N, Dim);
+            Transform t;
+            foreach (i; 0 .. minDimension)
+            {
+                foreach (j; 0 .. Dim)
+                {
+                    if (j != i)
+                    {
+                        t[j, i] = values[i];
+                    }
+                }
+            }
+            return t;
+        }
+        /// Apply shearing in-place.
+        /// Returns: this
+        ref Transform shear(uint N)(const auto ref T[N] values) return
+        {
+            return this.combine(CompactTransform.fromShearing(values));
+        }
+        /// Returns a sheared copy of Transform.
+        Transform sheared(uint N)(const auto ref T[N] values) const
+        {
+            Transform t = this;
+            return t.shear(values);
+        }
+
+        /// Constructs a new Transform representing a 2D rotation.
+        /// Params:
+        ///   angle = Rotation angle in radians
         static Transform fromRotation(const FT angle)
         {
             Transform t;
-            const auto c = cos(angle), s = sin(angle);
+            immutable auto c = cos(angle), s = sin(angle);
             t[0, 0] = c; t[0, 1] = -s;
             t[1, 0] = s; t[1, 1] = c;
             return t;
         }
+        /// Constructs a new Transform representing a 2D rotation.
+        /// Params:
+        ///   angle = Rotation angle in degrees
         static auto fromRotationDegrees(const FT degrees)
         {
             return fromRotation(degreesToRadians(degrees));
         }
+        /// Apply 2D rotation in-place.
+        /// Params:
+        ///   angle = Rotation angle in radians
+        /// Returns: this
         ref Transform rotate(const FT angle) return
         {
             return this.combine(CompactTransform.fromRotation(angle));
         }
+        /// Apply 2D rotation in-place.
+        /// Params:
+        ///   angle = Rotation angle in degrees
         auto rotateDegrees(const FT degrees)
         {
             return rotate(degreesToRadians(degrees));
         }
+        /// Returns a rotated copy of Transform.
+        /// Params:
+        ///   angle = Rotation angle in radians
         Transform rotated(const FT angle) const
         {
             Transform t = this;
             return t.rotate(angle);
         }
+        /// Returns a rotated copy of Transform.
+        /// Params:
+        ///   angle = Rotation angle in degrees
         auto rotatedDegrees(const FT degrees) const
         {
             return rotated(degreesToRadians(degrees));
@@ -230,62 +276,98 @@ if (Dim > 0)
     // 3D transforms
     static if (Dim >= 3)
     {
+        /// Constructs a new Transform representing a 3D rotation aroud the X axis.
+        /// Params:
+        ///   angle = Rotation angle in radians
         static Transform fromXRotation(const FT angle)
         {
             Transform t;
-            auto c = cos(angle), s = sin(angle);
+            immutable auto c = cos(angle), s = sin(angle);
             t[1, 1] = c; t[2, 1] = -s;
             t[1, 2] = s; t[2, 2] = c;
             return t;
         }
+        /// Constructs a new Transform representing a 3D rotation aroud the X axis.
+        /// Params:
+        ///   angle = Rotation angle in degrees
         static auto fromXRotationDegrees(const FT degrees)
         {
             return fromXRotation(degreesToRadians(degrees));
         }
+        /// Apply 3D rotation around the X axis in-place.
+        /// Params:
+        ///   angle = Rotation angle in radians
         ref Transform rotateX(const FT angle) return
         {
             return this.combine(CompactTransform.fromXRotation(angle));
         }
+        /// Apply 3D rotation around the X axis in-place.
+        /// Params:
+        ///   angle = Rotation angle in degrees
         auto rotateXDegrees(const FT degrees)
         {
             return rotateX(degreesToRadians(degrees));
         }
+        /// Returns a copy of Transform rotated around the X axis.
+        /// Params:
+        ///   angle = Rotation angle in radians
         Transform rotatedX(const FT angle) const
         {
             Transform t = this;
             return t.rotateX(angle);
         }
+        /// Returns a copy of Transform rotated around the X axis.
+        /// Params:
+        ///   angle = Rotation angle in degrees
         auto rotatedXDegrees(const FT degrees)
         {
             return rotatedX(degreesToRadians(degrees));
         }
 
 
+        /// Constructs a new Transform representing a 3D rotation aroud the Y axis.
+        /// Params:
+        ///   angle = Rotation angle in radians
         static Transform fromYRotation(const FT angle)
         {
             Transform t;
-            auto c = cos(angle), s = sin(angle);
+            immutable auto c = cos(angle), s = sin(angle);
             t[0, 0] = c; t[2, 0] = s;
             t[0, 2] = -s; t[2, 2] = c;
             return t;
         }
+        /// Constructs a new Transform representing a 3D rotation aroud the Y axis.
+        /// Params:
+        ///   angle = Rotation angle in degrees
         static auto fromYRotationDegrees(const FT degrees)
         {
             return fromYRotation(degreesToRadians(degrees));
         }
+        /// Apply 3D rotation around the Y axis in-place.
+        /// Params:
+        ///   angle = Rotation angle in radians
         ref Transform rotateY(const FT angle) return
         {
             return this.combine(CompactTransform.fromYRotation(angle));
         }
+        /// Apply 3D rotation around the Y axis in-place.
+        /// Params:
+        ///   angle = Rotation angle in degrees
         auto rotateYDegrees(const FT degrees)
         {
             return rotateY(degreesToRadians(degrees));
         }
+        /// Returns a copy of Transform rotated around the Y axis.
+        /// Params:
+        ///   angle = Rotation angle in radians
         Transform rotatedY(const FT angle) const
         {
             Transform t = this;
             return t.rotateY(angle);
         }
+        /// Returns a copy of Transform rotated around the Y axis.
+        /// Params:
+        ///   angle = Rotation angle in degrees
         auto rotatedYDegrees(const FT degrees)
         {
             return rotatedY(degreesToRadians(degrees));
@@ -301,14 +383,20 @@ if (Dim > 0)
     }
 }
 
-/// Pre-multiply transformation into target, returning a reference to target
-ref Transform!(T, Dim, C1) combine(T, uint Dim, bool C1, bool C2)(ref return Transform!(T, Dim, C1) target, const Transform!(T, Dim, C2) transformation)
+/// Pre-multiply `transformation` into `target`, returning a reference to `target`
+auto ref combine(T, uint Dim, TransformOptions O1, TransformOptions O2)(
+    ref return Transform!(T, Dim, O1) target,
+    const auto ref Transform!(T, Dim, O2) transformation
+)
 {
     target = target.combined(transformation);
     return target;
 }
-/// Returns the result of pre-multiplying transformation and target
-Transform!(T, Dim, C1) combined(T, uint Dim, bool C1, bool C2)(const Transform!(T, Dim, C1) target, const Transform!(T, Dim, C2) transformation)
+/// Returns the result of pre-multiplying `transformation` and `target`
+Transform!(T, Dim, O1) combined(T, uint Dim, TransformOptions O1, TransformOptions O2)(
+    const auto ref Transform!(T, Dim, O1) target,
+    const auto ref Transform!(T, Dim, O2) transformation
+)
 {
     // Just about matrix multiplication, but assuming last row is [0...0 1]
     typeof(return) result;
@@ -332,7 +420,7 @@ Transform!(T, Dim, C1) combined(T, uint Dim, bool C1, bool C2)(const Transform!(
 unittest
 {
     alias Transform2D = Transform!(float, 2);
-    alias Transform2DCompact = Transform!(float, 2, true);
+    alias Transform2DCompact = Transform!(float, 2, TransformOptions.compact);
     alias Transform3D = Transform!(float, 3);
-    alias Transform3DCompact = Transform!(float, 3, true);
+    alias Transform3DCompact = Transform!(float, 3, TransformOptions.compact);
 }
